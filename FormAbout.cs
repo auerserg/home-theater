bb38b1235ece53a321b470e5719457b1b93afaf3
auerserg <auerserg@gmail.com>
2020-02-19 20:26:16 +0200

    remove not used lib

--- FormAbout.cs
+++ FormAbout.cs
@@ -34,7 +34,7 @@ namespace HomeTheater
                 if (attributes.Length > 0)
                 {
                     var titleAttribute = (AssemblyTitleAttribute) attributes[0];
-                    if (titleAttribute.Title != "") return titleAttribute.Title;
+                    if (!string.IsNullOrEmpty(titleAttribute.Title)) return titleAttribute.Title;
                 }
 
                 return Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
